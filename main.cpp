2d8031c46af8045d511e94101d4dc280a1d61e1b
Fred Kozlowski <kozlowskifrederick@gmail.com>
2020-11-04 17:36:39 -0500

    added fundamental matrix calculation

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/features2d.hpp>
+#include <opencv2/calib3d.hpp>
 #include <iostream>
 
 int main(){
@@ -11,6 +12,8 @@ int main(){
   if(!cap.isOpened())
     return -1;
 
+  cv::Mat output;
+  cv::Mat fundamental;
   cv::Mat frame;
   cv::Mat grayframe;
   std::vector<cv::Point2f> corners;
@@ -21,7 +24,7 @@ int main(){
   cv::Ptr<cv::ORB> orb = cv::ORB::create();
   cv::Ptr<cv::DescriptorMatcher> bfmatch = cv::BFMatcher::create(cv::NORM_HAMMING, false);
   std::vector<std::vector<cv::DMatch>> matches;
-  std::vector<std::vector<cv::DMatch>> goodmatches;
+  std::vector<cv::DMatch> goodmatches;
   std::vector<cv::KeyPoint> kp;
   cv::Mat imgmatches;
   while(1){
@@ -46,12 +49,23 @@ int main(){
       bfmatch->knnMatch(desc, olddesc, matches, 2); 
     for(uint i = 0; i < matches.size(); ++i){
         if(matches[i][0].distance < 0.75 * matches[i][1].distance)
-          goodmatches.push_back(matches[i]/*[0]*/);
+          goodmatches.push_back(matches[i][0]);
     }
-    cv::Mat test;
+    std::vector<int> pointindex1;
+    std::vector<int> pointindex2;
+    for(std::vector<cv::DMatch>::const_iterator it = goodmatches.begin(); it != goodmatches.end(); ++it){
+      pointindex1.push_back(it->queryIdx);
+      pointindex2.push_back(it->trainIdx);
+    }
+    std::vector<cv::Point2f> selectpoints1;
+    std::vector<cv::Point2f> selectpoints2;
+    cv::KeyPoint::convert(kp, selectpoints1, pointindex1);
+    cv::KeyPoint::convert(oldkp, selectpoints2, pointindex2);
     if(!olddesc.empty()){
-      cv::drawMatches(oldgrayframe, oldkp, grayframe, kp, goodmatches, test);
-      imshow("vid", test);
+      fundamental = cv::findFundamentalMat(cv::Mat(selectpoints1), cv::Mat(selectpoints2), cv::FM_RANSAC);
+      std::cout << fundamental.size() << std::endl;
+      //cv::drawMatches(oldgrayframe, oldkp, grayframe, kp, goodmatches, output);
+      //imshow("vid", output);
     }
     oldgrayframe = grayframe;
     olddesc = desc;
